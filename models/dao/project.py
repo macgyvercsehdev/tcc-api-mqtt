1b8df279fc2349b710668e2dd5f99684dd29aa3f
joaovfg96 <joaovfg13@gmail.com>
2023-12-03 20:24:29 -0300

    Adicionando umidade

--- models/dao/project.py
+++ models/dao/project.py
@@ -3,6 +3,7 @@ PROJECT = {
     'id': {'$toString': '$_id'},
     'id_equipamento': 1,
     'temperatura': 1,
+    'umidade': 1,
     'data': {'$dateToString': {'format': '%Y-%m-%d %H:%M:%S', 'date': '$data'}},
     'dia_da_semana': 1,
     'consumo_diario': 1,
