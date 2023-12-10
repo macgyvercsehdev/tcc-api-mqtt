b078afa71e489af5229103cf6329844a53390edd
macgyvercseh-dev <macgyvercseh@outlook.com>
2023-12-09 22:11:55 -0300

    ajuste na data e hora

--- models/dao/project.py
+++ models/dao/project.py
@@ -3,9 +3,10 @@ PROJECT = {
     'id': {'$toString': '$_id'},
     'id_equipamento': 1,
     'temperatura': 1,
-    'data': {'$dateToString': {'format': '%Y-%m-%d %H:%M:%S', 'date': '$data'}},
+    'data': {'$dateToString': {'format': '%Y-%m-%dT%H:%M:%S', 'date': '$data'}},
     'dia_da_semana': 1,
     'consumo_diario': 1,
-    'vazao_litro_acumulada':1
+    'vazao_litro_acumulada':1,
+    'umidade': 1
 }
 # O project serve para definir as colunas que o mongo vai retornar na resposta
